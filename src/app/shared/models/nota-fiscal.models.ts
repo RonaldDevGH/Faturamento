f23976d142b9cd0b95b6aab38b00c7f6380deb00
RonaldDevGH <ronaldsilva6762@gmail.com>
2026-04-25 09:37:47 -0300

    Delete src directory

--- src/app/shared/models/nota-fiscal.models.ts
+++
@@ -1,8 +0,0 @@
-import { ItemNota } from "./item-nota.models";
-
-export class NotaFiscal {
-    seqNumero: number = 0;
-    status: string = '';
-    dataEmissao: string = '';
-    produtos: ItemNota[] = [];
-}
--- src/app/shared/models/produto.models.ts
+++
@@ -1,5 +0,0 @@
-export class Produto {
-    id: number = 0;
-    descricao: string = '';
-    saldo: number = 0;
-}
